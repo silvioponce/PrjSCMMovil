e5f89cf3d2b9f5a0cca7810e1985e707c930e0fd
sponce <silvioponce@hotmail.com>
2016-08-30 18:09:31 -0600

    Mi primer Comit

--- app/src/main/java/com/example/sponce/prjscmmovil/ActivityListaNinos.java
+++ app/src/main/java/com/example/sponce/prjscmmovil/ActivityListaNinos.java
@@ -4,8 +4,11 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import com.example.sponce.prjscmmovil.Fragment.FragmentListNinos;
+import com.example.sponce.prjscmmovil.Fragment.FragmentNinos;
 
 public class ActivityListaNinos extends AppCompatActivity {
 
@@ -16,21 +19,55 @@ public class ActivityListaNinos extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lista_ninos);
 
-       // ((Toolbar) findViewById(R.id.toolbar)).setTitle(getTitle());
+        // ((Toolbar) findViewById(R.id.toolbar)).setTitle(getTitle());
 
         // Verificación: ¿Existe el detalle en el layout?
         if (findViewById(R.id.contenedor_detalle_articulo) != null) {
             // Si es asi, entonces confirmar modo Master-Detail
             dosPaneles = true;
 
+
         }
 
         // Agregar fragmento de lista
         getSupportFragmentManager()
                 .beginTransaction()
-                .replace(R.id.contenedor_lista, FragmentListNinos.crear())
+                .replace(R.id.contenedor_lista, new FragmentNinos())
                 .commit();
 
     }
 
+    private void cargarFragmentoDetalle() {
+        //Bundle arguments = new Bundle();
+        //arguments.putString(FragmentNinos.ID_ARTICULO, id);
+        FragmentNinos fragment = new FragmentNinos();
+        //fragment.setArguments(arguments);
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.contenedor_detalle_articulo, fragment)
+                .commit();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_ninos, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.btnNuevo) {
+            if (dosPaneles) {
+                Object buscarNinosFragment = getFragmentManager().findFragmentByTag("FragmentNinos");
+                getSupportFragmentManager().beginTransaction().replace(R.id.contenedor_detalle_articulo, new FragmentNinos()).commit();
+
+            }
+        }
+
+
+        return super.onOptionsItemSelected(item);
+    }
 }
--- app/src/main/java/com/example/sponce/prjscmmovil/Fragment/FragmentListNinos.java
+++ app/src/main/java/com/example/sponce/prjscmmovil/Fragment/FragmentListNinos.java
@@ -6,6 +6,8 @@ import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
@@ -52,6 +54,11 @@ public class FragmentListNinos extends Fragment implements AdapterNinos.OnItemCl
         prepararLista((RecyclerView) recyclerView);
 
         return v;
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
     }
 
     private void prepararLista(@NonNull RecyclerView recyclerView) {
